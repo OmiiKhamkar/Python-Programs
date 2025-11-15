2e2943f4f056040ba5ac9bc3ccade6fc6189671b
Omkar Khamkar <omkar.khamkar18@gmail.com>
2025-11-15 19:07:43 +0530

    Functions of the List

---
+++ Fuctions_of_lists.py
@@ -0,0 +1,31 @@
+
+# Append fuction to add element in the end of the list
+list1 = ["Omkar","Dhiraj","Suraj",18]
+print(list1)
+list1.append("Vedant")
+print(list1)
+
+#Sort the list in Asending order
+list2 = [23,54,16,1,19,45]
+list2.sort()
+print(list2)
+
+# Reverse the list
+list3 = [23,54,16,1,19,45]
+list3.reverse()
+print(list3)
+
+# Insert the element in the List
+list4 = [23,54,16,1,19,45]
+list4.insert(2,11)
+print(list4)
+
+# Pop Function to Delete the element from given index no
+list5 = [23,54,16,1,19,45]
+list5.pop(2)
+print(list5)
+
+# Remove fuction to remove the given element from the index
+list6 = [23,54,16,1,19,45]
+list6.remove(1)
+print(list6)
