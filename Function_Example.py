3d9f4f83edab8a7934829ead17ea8c65b611a4ba
Omkar Khamkar <omkar.khamkar18@gmail.com>
2025-11-21 23:41:27 +0530

    Function Example

---
+++ Function_Example.py
@@ -0,0 +1,12 @@
+
+# Function Defination
+def avg():
+    n1 = int(input("Enter the first number : "))
+    n2 = int(input("Enter the second number : "))
+    n3 = int(input("Enter the third number : "))
+
+    average = (n1 + n2 + n3)/3
+    print(average)
+
+# Function Call
+avg()
