a55a49db64c3a010b37b32121d09f08f1d52c32e
Omkar Khamkar <omkar.khamkar18@gmail.com>
2025-11-22 23:33:14 +0530

    Recursion Function Example

---
+++ Recursion.py
@@ -0,0 +1,12 @@
+# Recursion Function for Factorial
+
+def factorial(n):
+   
+    if (n==0 or n==1):
+        return 1
+    else:
+        return n* factorial(n-1)
+
+n = int(input("Enter the number :"))
+print("factorial = ",factorial(n))
+
