05241b2ef885a3c6aba53ff3bc0fbdeb0a109208
Omkar Khamkar <omkar.khamkar18@gmail.com>
2025-11-25 23:39:47 +0530

    Count vowels in string

---
+++ Count_vowels_in_string.py
@@ -0,0 +1,18 @@
+
+def count_vowels(s):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for ch in s:
+        if ch in vowels:
+            count += 1
+    return count
+
+print(count_vowels("Python Programming"))
+
+
+
+
+
+
+
+
